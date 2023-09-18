0a6810f984524c5af00384f1a67b9cb5ca7b3762
dram_actic <deepakkumar24m@gmail.com>
2023-09-18 11:10:05 +0530

    2nd Commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import jessica from '../assests/jessica.png'
 
 export default function Header() {
 
@@ -17,21 +18,43 @@ export default function Header() {
   return (
     <header className="w-full h-auto  bg-black">
         <div className="flex flex-col justify-center">
-        <div className="flex justify-between items-end px-4 lg:mx-[2.5rem]">
-            <div className="text-white text-3xl md:text-4xl font-light">ShopKart</div>
-            <div className="hidden md:flex text-white font-semibold text-lg md:text-sm space-x-2">
-                <div className="cursor-pointer">WISHLIST (0)</div>
-                <div className="cursor-pointer">BAG (0)</div>
+            <div className="flex justify-between items-end px-4 lg:mx-[2.5rem]">
+                <div className="text-white text-3xl md:text-4xl font-light hover:cursor-pointer">ShopKart</div>
+                <div className="hidden md:flex text-white font-semibold md:text-sm gap-3">
+                    <div className="cursor-pointer">WISHLIST (0)</div>
+                    <div className="cursor-pointer">BAG (0)</div>
+                </div>
+                <div className="md:hidden text-white text-3xl">
+                    <a href="#">&#8801;</a>
+                </div>
             </div>
-            <div className="md:hidden text-white text-3xl">
-                <a href="#">&#8801;</a>
-            </div>
+            <div className="w-auto h-1 mt-1 border-b-[1px] border-white rounded-sm mx-[1.5rem] lg:mx-16"></div>
         </div>
-        <div className="w-auto h-1 mt-1 border-b-[1px] border-white rounded-sm mx-[1.5rem] lg:mx-16"></div>
-        </div>
-        <div>
-           <p className='text-white'>heloo</p>
-        </div>
+        <nav className="mt-2 hidden md:block">
+            <ul className="flex justify-evenly md:pr-32 text-lg font-medium break-word text-white uppercase">
+                {navbar.map((item) => (
+                    <li key={item.id} className="relative group">
+                        <a href={`#${item.id}`} className="text-white transition duration-300 ease-in-out">
+                            {item.name}
+                        </a>
+                        {item.child && (
+                            <ul className="absolute left-0 hidden group-hover:block bg-transparent border border-white py-2 space-y-2">
+                                {item.child.map((subItem) => (
+                                    <li key={subItem.id}>
+                                    <a href={`#${subItem.id}`} className="text-white text-sm font-light transition duration-300 ease-in-out pr-5 py-1 block">
+                                        {subItem.name}
+                                    </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </li>
+                ))}
+            </ul>
+      </nav>
+      <div>
+       <img src={jessica} alt="model" className="max-w-sm"/>
+      </div>
     </header>
   )
 }
--- src/component/Newslleter.js
+++ src/component/Newslleter.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React from 'react';
+import img from "../assests/newslleter-bg-img.png";
+import {GiCircle} from "react-icons/gi";
 
 export default function Newslleter() {
+    const divStyle = {
+        backgroundImage: `url(${img})`,
+        backgroundSize: "cover", // You can adjust the background size as needed
+        backgroundRepeat: "no-repeat", // You can adjust the background repeat behavior
+        // Add any other CSS properties you want for your div here
+        width: "auto", // Example width
+        height: "515px", // Example height
+      };
   return (
     <section className="">
-        <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url('')`}}>
-        <img src="./assests/newslleter-bg-img.png" alt="" />
+        <div className="bg-center bg-local" style={divStyle}>
+            <div className="container mx-auto">
+                <div className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-3 py-10 md:py-[5.5rem] md:gap-5">
+                    <div className="flex flex-col items-center md:items-start text-[#FFFFFF80]">
+                        <p className="text-[26px] font-semibold font-[Syne] break-words">Newslleter</p>
+                        <p className="text-lg font-normal break-words text-center">Get news about articles and <br className="md:hidden"/>updates in your inbox.</p>
+                    </div>
+                    <div class="grid grid-cols-1 gap-6 text-white">
+                        <label class="block">
+                            <input type="text" class="block w-full px-0.5 border-b-2 bg-transparent" placeholder="NAME"/>
+                        </label>
+                        <label class="block">
+                            <input type="email" class="block w-full px-0.5 border-b-2 bg-transparent" placeholder="EMAIL"/>
+                        </label>
+                        <label class="block">
+                            <textarea class="block w-full px-0.5 border-b-2 bg-transparent" placeholder="MESSAGE" rows="1"></textarea>
+                        </label>
+                    </div>
+                    {/* <div className="flex flex-col items-start text-[#FFFFFF80] text-lg font-normal md:col-span-2">
+                        <p className="">NAME</p>
+                        <p className="">EMAIL</p>
+                        <p className="">MESSAGE</p>
+                    </div> */}
+
+                    {/* <div className="relative">
+                    <div className="flex justify-between items-start">
+                        <p className="z-10 relative">djsandsjdsajddjnsajdnsajdsdjsddnsjdsjdnsdjsnjsnjn</p>
+                        <div className="absolute top-0 right-0">
+                        <GiCircle className="w-10 h-10 text-[#F07E15]" />
+                        </div>
+                    </div>
+                    </div> */}
+                </div>
+                    <div className="text-white text-[55px] md:text-[135px] font-[Syne] font-extrabold leading-[37.18px] md:leading-[102.60px] tracking-wider break-words mt-4 md:mt-[1.5rem]">GET <br/>IN <br className="md:hidden"/>TOUCH</div>
+                
+                </div>
         </div>
     </section>
   )
